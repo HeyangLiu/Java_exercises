4f2377d25044fd638b6e7b2190b34724fedb18c3
HeyangLiu <60200308+HeyangLiu@users.noreply.github.com>
2021-06-07 13:06:04 -0700

    add q20

--- leetcode.java
+++ leetcode.java
@@ -2,4 +2,35 @@ class leetcode {
     public static void main(String[] args) {
         System.out.print("hello");
     }
+    //20. Valid Parentheses
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        stack.push(s.charAt(0));
+        for(int i=1; i<s.length(); i++){
+            if(s.charAt(i)==')'){
+                if(!stack.empty()&&stack.peek()=='(')
+                    stack.pop();
+                else
+                    return false;
+            }
+            else if(s.charAt(i)==']'){
+                if(!stack.empty()&&stack.peek()=='[')
+                    stack.pop();
+                else
+                    return false;
+            }
+            else if(s.charAt(i)=='}'){
+                if(!stack.empty()&&stack.peek()=='{')
+                    stack.pop();
+                else
+                    return false;
+            }
+            else
+                stack.push(s.charAt(i));
+        }
+        if(stack.isEmpty()){
+            return true;
+        }
+        return false;
+    }
 }
