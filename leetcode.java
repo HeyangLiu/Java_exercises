bd9219de42e261b62f398a184798ecf73b842a42
HeyangLiu <60200308+HeyangLiu@users.noreply.github.com>
2021-06-07 14:10:46 -0700

    add q21

--- leetcode.java
+++ leetcode.java
@@ -33,4 +33,25 @@ class leetcode {
         }
         return false;
     }
+    //21. Merge Two Sorted Lists
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1==null) return l2;
+        if(l2==null) return l1;
+        ListNode root = new ListNode();
+        ListNode head = root;
+        while(l1!=null&&l2!=null){
+            if(l1.val > l2.val){
+                root.next=l2;
+                l2=l2.next;
+            }
+            else{
+                root.next=l1;
+                l1=l1.next;
+            }
+            root=root.next;
+        }
+        if(l1!=null) root.next=l1;
+        if(l2!=null) root.next=l2;
+        return head.next;
+    }
 }
