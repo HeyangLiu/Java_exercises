e62f362129ef9a0c1c8df17d00fbe2ffb2db487e
HeyangLiu <60200308+HeyangLiu@users.noreply.github.com>
2021-06-11 23:07:38 -0700

    add q88

--- leetcode.java
+++ leetcode.java
@@ -2,6 +2,33 @@ class leetcode {
     public static void main(String[] args) {
         System.out.print("hello");
     }
+    //88. Merge Sorted Array
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int [] mer = new int[m+n];
+        int i=0, j=0, k=0;
+        while(i<m&&j<n){
+            if(nums1[i]<=nums2[j]){
+                mer[k]=nums1[i];
+                i++;
+            }
+            else{
+                mer[k]=nums2[j];
+                j++;
+            }
+            k++;
+        }
+        for(;i<m; i++){
+            mer[k]=nums1[i];
+            k++;
+        }
+        for(;j<n; j++){
+            mer[k]=nums2[j];
+            k++;
+        }
+        for(i=0; i<m+n; i++){
+            nums1[i]=mer[i];
+        }
+    }
     //83. Remove Duplicates from Sorted List
     public ListNode deleteDuplicates(ListNode head) {
         ListNode cur = head;
