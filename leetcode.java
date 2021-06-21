b878ef9df178109edf81a5efffe7d80630130053
HeyangLiu <60200308+HeyangLiu@users.noreply.github.com>
2021-06-20 21:57:16 -0700

    add q263

--- leetcode.java
+++ leetcode.java
@@ -2,6 +2,21 @@ class leetcode {
     public static void main(String[] args) {
         System.out.print("hello");
     }
+    //263. Ugly Number
+    public boolean isUgly(int n) {
+        if(n==0) return false;
+        if(n==1) return true;
+        while(n%2==0){
+            n=n/2;
+        }
+        while(n%3==0){
+            n=n/3;
+        }
+        while(n%5==0){
+            n=n/5;
+        }
+        return n==1;
+    }
     //258. Add Digits
     public int addDigits(int n) {
         if(n==0) return 0;
