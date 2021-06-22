46b55255aa3fbfa79c88e0bd6ccf5bda6f39bb78
HeyangLiu <60200308+HeyangLiu@users.noreply.github.com>
2021-06-21 23:18:05 -0700

    add q278

--- leetcode.java
+++ leetcode.java
@@ -2,6 +2,23 @@ class leetcode {
     public static void main(String[] args) {
         System.out.print("hello");
     }
+    //278. First Bad Version
+    public int firstBadVersion(int n) {
+        int l=1, r=n;
+        int mid;
+        while(l<r){
+            mid=l+(r-l)/2;//instead of (l+r)/2 to avoid overflow
+            if(isBadVersion(mid)){
+                
+                r=mid;
+            }
+            else{
+                l=mid+1;
+            }
+            
+        }
+        return l;
+    }
     //338. Counting Bits
     public int[] countBits(int n) {
         int[] ans = new int [n+1];
