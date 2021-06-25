cee6a8ae9a4cd9048e21425c55f811b04553c4f5
HeyangLiu <60200308+HeyangLiu@users.noreply.github.com>
2021-06-24 23:14:13 -0700

    add q94

--- dfs.java
+++ dfs.java
@@ -1,1 +1,15 @@
-
+class dfs{
+    //94. Binary Tree Inorder Traversal
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<Integer>();
+        helper(ans, root);
+        return ans;
+    }
+    public void helper(List<Integer> a, TreeNode n){
+        if(n==null) return;
+        helper(a, n.left);
+        a.add(n.val);
+        helper(a, n.right);
+    }
+    
+}
