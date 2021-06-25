8d795b4b416d707ac6e69b902b1b7b73d5269275
HeyangLiu <60200308+HeyangLiu@users.noreply.github.com>
2021-06-24 23:30:16 -0700

    add q98

--- dfs.java
+++ dfs.java
@@ -11,5 +11,20 @@ class dfs{
         a.add(n.val);
         helper(a, n.right);
     }
+    //98. Validate Binary Search Tree
+    public boolean isValidBST(TreeNode root) {
+        List<Integer> a = new ArrayList<Integer>();
+        helper(a, root);
+        for(int i=1; i<a.size(); i++){
+            if(a.get(i)<=a.get(i-1)) return false;
+        }
+        return true;
+    }
+    public void helper(List<Integer> x, TreeNode n){
+        if(n==null) return;
+        helper(x, n.left);
+        x.add(n.val);
+        helper(x, n.right);
+    }
     
 }
